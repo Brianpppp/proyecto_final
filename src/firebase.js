e9f76dc90368714ff4578fa79cebb12aba322c88
brian <bapipi@prateducacio.com>
2024-01-18 17:00:36 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClrqorNhgKunz_vRQKywJ9ejPgoqmI3DU",
+  authDomain: "proyectodam-c3d01.firebaseapp.com",
+  projectId: "proyectodam-c3d01",
+  storageBucket: "proyectodam-c3d01.appspot.com",
+  messagingSenderId: "918395564259",
+  appId: "1:918395564259:web:cf8d571564faa3feec95d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
